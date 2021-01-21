81a05f653315f56207b1787731345d86f55714fc
Roberto Pullia <roberto.pullia@internet-idee.net>
2021-01-21 12:14:26 +0100

    Added a json file as response

--- src/main/java/org/example/rest/HelloRestService.java
+++ src/main/java/org/example/rest/HelloRestService.java
@@ -7,6 +7,15 @@ import javax.ws.rs.core.Response;
 @Path("/") //this specify a base path
 
 public class HelloRestService {
+
+    String json = "{\n" +
+            "\t\"email\": \"email@example.com\",\n" +
+            "\t\"timestamp\": {\n" +
+            "\t\t\"$date\": \"2021-01-20T16:59:20.801Z\"\n" +
+            "\t},\n" +
+            "\t\"version\": \"2.0\",\n" +
+            "\t\"path\": \"my.package.className\"\n} ";
+
     @GET //this annotation indicates GET request and the code to execute if we get a GET request
 
     //the endpoint to utilize
@@ -14,6 +23,6 @@ public class HelloRestService {
 
     public Response hello(){
         //given the path "/hello" reply back with a respons with 200 status code OK and "hello"
-        return Response.status(200).entity("hello").build();
+        return Response.status(200).entity(json).build();
     }
 }
